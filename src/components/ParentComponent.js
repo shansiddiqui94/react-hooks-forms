89fa28f2681f69f61f3c7c40102993bb34549064
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-03-06 02:28:34 -0500

    completed

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Form from "./Form";
+
+function ParentComponent() {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
+  return (
+<div>
+    <Form
+      firstName={firstName}
+      lastName={lastName}
+      handleFirstNameChange={handleFirstNameChange}
+      handleLastNameChange={handleLastNameChange}
+    />
+</div>
+  );
+}
+
+export default ParentComponent
